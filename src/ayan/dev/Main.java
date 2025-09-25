8e4e49c807de1abaf886ea7f70c678ea15888200
Syed Ayan <syedayan4128@gmail.com>
2025-09-25 20:37:21 +0530

    adding the user register and  login methods in Main.java

--- src/ayan/dev/Main.java
+++ src/ayan/dev/Main.java
@@ -7,9 +7,20 @@ public class Main {
 
         if (DBConnection.getConnection()!=null) {
             System.out.println("DataBase Connect successfully!!!");
+
+            //testing the user...
+            User user = new User(0,"Ayan","example123@email.com","example12345");
+            UserDAO.registerUser(user);
+
+
+            //testing the login user....
+            UserDAO.loginUser("example123@email.com","example12345"); // correct detail
+            UserDAO.loginUser("example123@email.com","example12345"); // incorrect detail
+
         }else{
             System.out.println("DataBase Not Connect :( ");
         }
+
 
     }
 }
