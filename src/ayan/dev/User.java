e01d7ca729ad1e52bf48b16c04edae622b19d4cc
Syed Ayan <syedayan4128@gmail.com>
2025-09-25 20:26:42 +0530

    add more logic and code

--- src/ayan/dev/User.java
+++ src/ayan/dev/User.java
@@ -12,4 +12,20 @@ public class User {
         this.email = email;
         this.password = password;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
--- src/ayan/dev/UserDAO.java
+++ src/ayan/dev/UserDAO.java
@@ -2,6 +2,7 @@ package ayan.dev;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class UserDAO {
     public static void registerUser(User user){
@@ -13,10 +14,36 @@ public class UserDAO {
             ps.setString(2,user.email);
             ps.setString(3,user.password);
 
-            ps.executeUpdate();
-            System.out.println("User Registered Successfully!! :) ");
+            int row = ps.executeUpdate();
+            if (row > 0) {
+                System.out.println("User Registered Successfully!! :) ");
+
+            }
         } catch (Exception e) {
-            System.out.println("Error : " +e.getMessage());
+            e.printStackTrace();
         }
+    }
+
+    // Login User
+    public static boolean loginUser(String email, String password) {
+        String query = "SELECT * FROM users WHERE email=? AND password=?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            stmt.setString(1, email);
+            stmt.setString(2, password);
+
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                System.out.println("Login successful 🎉");
+                return true;
+            } else {
+                System.out.println("Invalid email or password ❌");
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
