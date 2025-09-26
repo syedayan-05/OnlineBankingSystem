5dda32263e1c3c7478bbdeda3c45ce08187e02d9
Syed Ayan <syedayan4128@gmail.com>
2025-09-27 01:48:30 +0530

    create register fxml for RegisterController and Register

--- src/ayan/dev/UserDAO.java
+++ src/ayan/dev/UserDAO.java
@@ -3,7 +3,7 @@ package ayan.dev;
 import java.sql.*;
 
 public class UserDAO {
-    private Connection conn;
+    private static Connection conn;
 
     public UserDAO() {
         try {
@@ -29,7 +29,7 @@ public class UserDAO {
     }
 
     // ✅ Login User
-    public boolean loginUser(String email, String password) {
+    public static boolean loginUser(String email, String password) {
         String query = "SELECT * FROM users WHERE email=? AND password=?";
         try (PreparedStatement ps = conn.prepareStatement(query)) {
             ps.setString(1, email);
