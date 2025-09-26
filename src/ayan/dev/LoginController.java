549959eb0984b95f193e9859ce25137a9036a6ed
Syed Ayan <syedayan4128@gmail.com>
2025-09-27 01:44:41 +0530

    create LoginController for user login in system

--- src/ayan/dev/LoginController.java
+++ src/ayan/dev/LoginController.java
@@ -1,4 +1,30 @@
 package ayan.dev;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    @FXML
+    private TextField emailField;
+    @FXML
+    private PasswordField passwordField;
+
+    public void handleLogin(ActionEvent event) {
+        String email = emailField.getText();
+        String pass = passwordField.getText();
+
+        if (UserDAO.loginUser(email, pass)) {
+            System.out.println("Login Successful ✅");
+            // TODO: Load dashboard.fxml
+        } else {
+            System.out.println("Invalid Email or Password ❌");
+        }
+    }
+
+    public void openRegister(ActionEvent event) {
+        System.out.println("Open Register Page...");
+        // TODO: Switch scene to register.fxml
+    }
 }
