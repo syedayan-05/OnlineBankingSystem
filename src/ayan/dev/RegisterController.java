4cca8f49921e6fd5859e570444247d3e91016f52
Syed Ayan <syedayan4128@gmail.com>
2025-09-27 01:55:41 +0530

    create a RegisterController for user Register in Sytem

--- src/ayan/dev/RegisterController.java
+++ src/ayan/dev/RegisterController.java
@@ -1,4 +1,20 @@
 package ayan.dev;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class RegisterController {
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField passwordField;
+
+    public void handleRegister(ActionEvent event) {
+        User user =new User(0,nameField.getText(),emailField.getText(),passwordField.getText());
+        UserDAO.registerUser(user);
+        System.out.println("User Registered Successfully ");
+    }
 }
