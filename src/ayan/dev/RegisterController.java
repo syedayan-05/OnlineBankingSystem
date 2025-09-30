c512ee5d81353b652cc07f58c14daf1fe2d8dcae
Syed Ayan <syedayan4128@gmail.com>
2025-09-30 21:04:59 +0530

    adding the new FXMLLoader in RegisterController

--- src/ayan/dev/RegisterController.java
+++ src/ayan/dev/RegisterController.java
@@ -2,8 +2,11 @@ package ayan.dev;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class RegisterController {
     @FXML
@@ -21,8 +24,19 @@ public class RegisterController {
         UserDAO userDAO = new UserDAO();
         userDAO.registerUser(name, email, password);
 
-
         System.out.println("User Registered Successfully ✅");
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ayan/dev/view/dashboard.fxml"));
+            Scene scene = new Scene(loader.load(),800,600);
+
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Dashboard-Online Banking System");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
