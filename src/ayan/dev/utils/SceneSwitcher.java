27eb67c19972c672ca200724afe83857a6138bb2
Syed Ayan <syedayan4128@gmail.com>
2025-10-08 00:58:21 +0530

    "Added Dashboard, SceneSwitcher, and fully functional Login/Register flow"

---
+++ src/ayan/dev/utils/SceneSwitcher.java
@@ -0,0 +1,23 @@
+package ayan.dev.utils;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneSwitcher {
+
+    public static void switchScene(ActionEvent event, String fxmlPath, String title) throws IOException {
+        FXMLLoader loader = new FXMLLoader(SceneSwitcher.class.getResource(fxmlPath));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle(title);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
